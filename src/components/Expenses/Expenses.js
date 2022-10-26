8a33c748caa1752c482d9cd6646e23be78c4cd7f
Vasco Junior <vasco.fernandes.bueno@gmail.com>
2022-10-26 16:08:22 -0300

    last commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
-import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 import "./Expenses.css";
 
@@ -16,7 +17,7 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter((expense) => {
     return (
       expense.date.getFullYear().toString() === filteredYear ||
-      filteredYear == ""
+      filteredYear === ""
     );
   });
 
@@ -26,6 +27,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
